d30bd4fcc66b4db9a19b9158ea8e69eacb9b9275
Marcin Wrona <marcin.wrona1@pollub.edu.pl>
2022-12-02 19:13:52 +0100

    Dodanie przykładowej głównej klasy i funkcji ('main'), konfiguracja Maven'a dla kompilacji do pliku '.jar'

---
+++ src/main/java/com/asynchronousboiz/pwo_project/MainClass.java
@@ -0,0 +1,13 @@
+package com.asynchronousboiz.pwo_project;
+
+/**
+ *
+ * @author Marcin Wrona
+ */
+public class MainClass {
+    public static void main (String[] argv) {
+        System.out.println("Metoda main() nie zaimplementowana");
+
+        System.exit(0);
+    }
+}
