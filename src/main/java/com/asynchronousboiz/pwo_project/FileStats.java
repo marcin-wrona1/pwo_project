42035a4a6249ccf4bb5be7b6307e435ad0cc99ff
Jakub Kozłowski <jakub.kozlowski2@pollub.edu.pl>
2022-12-10 22:21:41 +0100

    FileStats: Implementacja obliczania rozmiaru pliku

--- src/main/java/com/asynchronousboiz/pwo_project/FileStats.java
+++ src/main/java/com/asynchronousboiz/pwo_project/FileStats.java
@@ -1,9 +1,33 @@
 package com.asynchronousboiz.pwo_project;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  *
  * @author Jakub Kozłowski
  */
 public class FileStats {
+    /**
+     * Pobiera rozmiar pliku podanego jako parametr.
+     *
+     * @param filepath ścieżka do pliku
+     * @return rozmiar pliku
+     * @throws IllegalArgumentException Jeśli podany parametr jest nieprawidłowy
+     * @throws IOException Jeśli wystąpił błąd dostępu do podanej ścieżki
+     */
+    public static long fileSize(String filepath) throws IllegalArgumentException, IOException {
+        if (filepath == null) {
+            throw new IllegalArgumentException("Nie podano ścieżki");
+        }
 
+        Path path = Paths.get(filepath);
+        if (!Files.isRegularFile(path)) {
+            throw new IllegalArgumentException("Podano nieprawidłowy typ pliku");
+        }
+
+        return Files.size(path);
+    }
 }
