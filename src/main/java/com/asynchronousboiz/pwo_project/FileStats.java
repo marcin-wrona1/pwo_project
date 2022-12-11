1b9c6ae311d378b0815b0a718b455f5cc8968fdc
Albert Strzyżewski <albert.strzyzewski@pollub.edu.pl>
2022-12-11 18:47:34 +0100

    FileStats: Implementacja liczenia znaków w pliku

--- src/main/java/com/asynchronousboiz/pwo_project/FileStats.java
+++ src/main/java/com/asynchronousboiz/pwo_project/FileStats.java
@@ -1,9 +1,42 @@
 package com.asynchronousboiz.pwo_project;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  *
  * @author Albert Strzyżewski
  */
 public class FileStats {
+    /**
+     * Pobiera ilość znaków w podanym pliku.
+     *
+     * @param filepath ścieżka do pliku
+     * @return ilość znaków w podanym pliku
+     * @throws IllegalArgumentException Jeśli podano nieprawidłowy parametr
+     * @throws IOException Jeśli wystąpi błąd przy dostępie do pliku
+     */
+    public static long characterCount(String filepath) throws IllegalArgumentException, IOException {
+        if (filepath == null) {
+            throw new IllegalArgumentException("Nie podano ścieżki");
+        }
 
+        Path path = Paths.get(filepath);
+        if (!Files.isRegularFile(path)) {
+            throw new IllegalArgumentException("Podano nieprawidłowy typ pliku");
+        }
+
+        long ret = 0;
+
+        BufferedReader br = Files.newBufferedReader(path);
+        String line;
+        while ((line = br.readLine()) != null) {
+            ret += line.length();
+        }
+
+        return ret;
+    }
 }
