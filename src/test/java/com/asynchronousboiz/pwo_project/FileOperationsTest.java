df277229f4f384a773b307dae4c06715cb22090c
Marcin Wrona <marcin.wrona1@pollub.edu.pl>
2022-12-11 20:15:48 +0100

    FileOperationsTest: Utworzenie klasy

---
+++ src/test/java/com/asynchronousboiz/pwo_project/FileOperationsTest.java
@@ -0,0 +1,34 @@
+package com.asynchronousboiz.pwo_project;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Marcin Wrona
+ */
+public class FileOperationsTest {
+    
+    public FileOperationsTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+}
