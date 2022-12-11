45110370de518795f025a331130ffae7eb2f8120
Marcin Wrona <marcin.wrona1@pollub.edu.pl>
2022-12-11 20:36:42 +0100

    FileOperationsTest: Implementacja testów (testu dla 'directoryContent')

--- src/test/java/com/asynchronousboiz/pwo_project/FileOperationsTest.java
+++ src/test/java/com/asynchronousboiz/pwo_project/FileOperationsTest.java
@@ -1,5 +1,10 @@
 package com.asynchronousboiz.pwo_project;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Marcin Wrona
  */
 public class FileOperationsTest {
+    private String testDirPath;
     
     public FileOperationsTest() {
     }
@@ -26,9 +32,38 @@ public class FileOperationsTest {
     
     @BeforeEach
     public void setUp() {
+        try {
+            Path testDir = Files.createTempDirectory("FileOperationsTest");
+            testDirPath = testDir.toAbsolutePath().normalize().toString();
+
+            int i;
+            for (i = 0; i < 10; i++) {
+                Files.createFile(Paths.get(testDir + "/file" + i));
+            }
+        } catch (IOException ex) {
+            assert(false);
+        }
     }
     
     @AfterEach
     public void tearDown() {
+    }
+
+    /**
+     * Test dla metody 'directoryContent', z klasy 'FileOperations'.
+     */
+    @Test
+    public void directoryContentTest() throws Exception {
+        List<Path> files = FileOperations.directoryContent(testDirPath);
+
+        assertEquals(10, files.size());
+        int i;
+        for (i = 0; i < 10; i++) {
+            String targetPath = testDirPath + "/file" + i;
+            Path file = files.get(i);
+            assertNotEquals(null, file);
+            String path = file.toAbsolutePath().normalize().toString();
+            assertEquals(targetPath, path);
+        }
     }
 }
