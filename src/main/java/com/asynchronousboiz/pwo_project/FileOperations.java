3a093804cf45f621baa0d45be1ab7b73eee1160c
Krzysztof Zarębski <krzysztof.zarebski@pollub.edu.pl>
2022-12-11 19:33:35 +0100

    FileOperations/moveFile: Implementacja metody

--- src/main/java/com/asynchronousboiz/pwo_project/FileOperations.java
+++ src/main/java/com/asynchronousboiz/pwo_project/FileOperations.java
@@ -36,4 +36,30 @@ public class FileOperations {
 
         Files.copy(from_path, to_path, StandardCopyOption.REPLACE_EXISTING);
     }
+
+    /**
+     * Przenosi plik z podanej ścieżki źródłowej do podanej ścieżki docelowej.
+     *
+     * @param from ścieżka źródłowa
+     * @param to ścieżka docelowa
+     * @throws IllegalArgumentException Podany parametr jest nieprawidłowy
+     * @throws IOException Błąd dostępu do podanej ścieżki
+     */
+    public static void moveFile(String from, String to) throws IllegalArgumentException, IOException {
+        if (from == null || to == null) {
+            throw new IllegalArgumentException("Nie podano ścieżki źródłowej lub docelowej");
+        }
+
+        Path from_path = Paths.get(from);
+        if (!Files.isRegularFile(from_path)) {
+            throw new IllegalArgumentException("Podano nieprawidłowy typ pliku");
+        }
+
+        Path to_path = Paths.get(to);
+        if (Files.exists(to_path)) {
+            throw new IllegalArgumentException("Plik docelowy już istnieje");
+        }
+
+        Files.move(from_path, to_path, StandardCopyOption.ATOMIC_MOVE, StandardCopyOption.REPLACE_EXISTING);
+    }
 }
